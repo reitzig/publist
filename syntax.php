51f81ef5fb0dbc86c9fbb8a009a1333101f96c8d
Raphael Reitzig <code@verrech.net>
2013-03-10 16:14:31 +0100

    Initial commit of old version

---
+++ syntax.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * DokuWiki Plugin publist (Syntax Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Raphael Reitzig <code@verrech.net>
+ */
+
+// must be run within Dokuwiki
+if (!defined('DOKU_INC')) die();
+
+if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
+if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+
+require_once DOKU_PLUGIN.'syntax.php';
+
+class syntax_plugin_publist extends DokuWiki_Syntax_Plugin {
+    function getType() {
+        return 'substition';
+    }
+
+    function getPType() {
+        return 'block';
+    }
+
+    function getSort() {
+        return 105;
+    }
+
+
+    function connectTo($mode) {
+        $this->Lexer->addSpecialPattern('\[publist\|.+?\]',$mode,'plugin_publist');
+    }
+
+    function handle($match, $state, $pos, &$handler){
+        $data = array();
+
+        // Partition properly 
+        $matches = array();
+        $pattern = '/\[publist(?:\|(page|file):(.+?))(?:\|(wiki|html):(page|file):(.+?))(?:\|(.+?(?:\|.+?)*))?\]/';
+        if ( 0 === preg_match($pattern, $match, $matches) ) {
+            $data['error'] = 'Not valid publist syntax: '.$match;
+        }
+        else {
+            $data['bibtex'] = array('type' => $matches[1], 'ref' => $matches[2]);
+            $data['template'] = array('target' => $matches[3], 'type' => $matches[4], 'ref' => $matches[5]);
+
+            $data['options'] = array();
+            if ( !empty($matches[6]) ) {
+               $matches = explode('|', $matches[6]);
+               foreach ( $matches as $opt ) {
+                 $optparts = array();
+                 if ( preg_match('/(.+?):(.+)/', $opt, $optparts) ) {
+                    $optparts[2] = explode(';', $optparts[2]);
+                    if ( count($optparts[2]) == 1 ) {
+                        $optparts[2] = $optparts[2][0];
+                    }
+                    else {
+                        // For complicated options like 'only', we have to explode deeper
+                        $option = array();
+                        foreach ( $optparts[2] as $single ) {
+                           $single = explode('=', $single);
+                           $option[$single[0]] = str_replace(',', '|', $single[1]);
+                        }
+                        $optparts[2] = $option;
+                    }
+                    $data['options'][$optparts[1]] = $optparts[2];
+                 }
+               }
+            }
+        }
+        
+        return $data;
+    }
+
+    function render($mode, &$renderer, $data) {
+        if($mode != 'xhtml') return false;
+ 
+        if ( empty($data['error']) ) {
+            // Retrieve BibTeX source
+            $bibtex = $this->_load($data, 'bibtex');
+            if ( empty($bibtex) ) {
+                $data['error'] .= $data['bibtex']['type'].' '.$data['bibtex']['ref'].' does not exist<br />';
+            }
+            
+            // Retrieve Template source
+            $template = $this->_load($data, 'template');
+            if ( empty($template) ) {
+                $data['error'] .= $data['template']['type'].' '.$data['template']['ref'].' does not exist<br />';
+            }
+            
+            if ( !empty($bibtex) && !empty($template) ) {
+                require_once(dirname(__FILE__).'/bib2tpl/bibtex_converter.php');
+                $parser = new BibtexConverter($data['options']);
+                $code = $parser->convert($bibtex, $template);
+                
+                if ( $data['template']['target'] == 'wiki' ) {
+                    $code = p_render($mode, p_get_instructions($code), $info);
+                }
+
+                $renderer->doc .= $code;
+            }
+        }
+        
+        if ( !empty($data['error']) ) {
+            $renderer->doc .= $data['error'];
+        }
+       
+        $renderer->info['cache'] = false;
+        
+        return true;
+    }
+
+    function _load($data, $kind) {
+        global $INFO;
+
+        if ( $data[$kind]['type'] == 'file' ) {
+            return file_get_contents(dirname(__FILE__).'/'.$kind.'/'.$data[$kind]['ref']);
+        }
+        else if ( $data[$kind]['type'] == 'page' ) {
+            $exists = false;
+            $id = $data[$kind]['ref'];
+            resolve_pageid($INFO['namespace'], &$id, &$exists);
+            if ( $exists ) {
+                return rawWiki($id);
+            }
+        }
+
+        return null;
+    }
+}
+
+// vim:ts=4:sw=4:et:enc=utf-8:
---
+++ template/xhtml_simple.tpl
@@ -0,0 +1,10 @@
+@{group@
+<h3>@groupkey@ (@groupcount@)</h3>
+<ul>
+  @{entry@
+  <li>
+    @author@, <strong>@title@</strong>, @year@
+  </li>
+  @}entry@
+</ul>
+@}group@
